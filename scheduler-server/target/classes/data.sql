92b6d114c84613d9374f44415b23be1327d7a30e
i544113 <tamara.zdravkovic@sap.com>
2022-07-30 12:42:03 +0200

    Fix problems after cloning project in IntelliJ

--- scheduler-server/target/classes/data.sql
+++ scheduler-server/target/classes/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO schedule_task (id, name, recurrency, code) VALUES (hibernate_sequence.nextval,'Task1','*/15 * * * * *','(1..10).each{ println "Hello World" sleep(50) }');
-INSERT INTO schedule_task (id, name, recurrency, code) VALUES (hibernate_sequence.nextval,'Task2','*/10 * * * * *','(1..10).each{ println "Hello World" sleep(100) }');
+INSERT INTO schedule_task (name, recurrency, code) VALUES ('Task1','*/15 * * * * *','(1..10).each{ println "Hello World" sleep(50) }');
+INSERT INTO schedule_task (name, recurrency, code) VALUES ('Task2','*/10 * * * * *','(1..10).each{ println "Hello World" sleep(100) }');
